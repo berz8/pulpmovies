b856202da551e40f1452cd64a6d43c7a02fcc7f7
Davide Lombardi <davidelombardi130p@gmail.com>
2024-10-12 21:12:40 +0200

    fix: svg jsx properties

--- apps/web/components/logo.tsx
+++ apps/web/components/logo.tsx
@@ -21,32 +21,32 @@ export default function Logo() {
       />
       <g filter="url(#filter0_d_19_91)">
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M196.62 32.0171C190.744 26.2475 182.852 23.4566 173.551 25.5576C148.47 31.2234 115.032 44.9507 81.4194 77.3992C47.5161 110.128 32.4785 144.966 25.8759 171.652C23.4649 181.396 26.3131 189.83 32.3552 196.115C38.5801 202.59 48.4551 206.951 59.8013 206.951H162.759C187.077 206.951 206.792 187.38 206.792 163.237V58.2426C206.792 47.4247 202.702 37.9893 196.62 32.0171ZM212.103 16.4775C222.761 26.9433 228.808 42.1624 228.808 58.2426V163.237C228.808 199.451 199.237 228.808 162.759 228.808H59.8013C42.9391 228.808 27.1417 222.349 16.4286 211.206C5.53267 199.872 0.177203 183.889 4.49514 166.438C11.9087 136.475 28.7292 97.7779 66.0707 61.7295C102.868 26.2061 140.048 10.7103 168.667 4.24552C185.785 0.378496 201.238 5.80909 212.103 16.4775Z"
           fill="#F8F8F8"
         />
       </g>
       <g filter="url(#filter1_d_19_91)">
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M32.0171 297.913C26.2475 303.789 23.4566 311.681 25.5576 320.982C31.2234 346.064 44.9507 379.502 77.3992 413.114C110.128 447.017 144.966 462.055 171.652 468.658C181.396 471.069 189.83 468.22 196.115 462.178C202.59 455.953 206.951 446.078 206.951 434.732L206.951 331.774C206.951 307.456 187.38 287.742 163.237 287.742L58.2426 287.742C47.4247 287.742 37.9893 291.831 32.0171 297.913ZM16.4775 282.431C26.9433 271.772 42.1624 265.726 58.2426 265.726L163.237 265.726C199.451 265.726 228.808 295.297 228.808 331.774L228.808 434.732C228.808 451.594 222.349 467.392 211.206 478.105C199.872 489.001 183.889 494.356 166.438 490.038C136.475 482.625 97.7779 465.804 61.7295 428.463C26.2061 391.665 10.7103 354.485 4.24551 325.867C0.378487 308.749 5.80908 293.296 16.4775 282.431Z"
           fill="#F8F8F8"
         />
       </g>
       <g filter="url(#filter2_d_19_91)">
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M462.516 196.62C468.286 190.745 471.077 182.852 468.976 173.551C463.31 148.47 449.583 115.032 417.134 81.4194C384.405 47.5162 349.567 32.4785 322.882 25.8759C313.138 23.4649 304.703 26.3131 298.418 32.3552C291.943 38.5801 287.582 48.4551 287.582 59.8013L287.582 162.759C287.582 187.078 307.154 206.792 331.296 206.792L436.291 206.792C447.109 206.792 456.544 202.702 462.516 196.62ZM478.056 212.103C467.59 222.761 452.371 228.808 436.291 228.808L331.296 228.808C295.082 228.808 265.726 199.237 265.726 162.759L265.726 59.8013C265.726 42.9391 272.184 27.1417 283.328 16.4286C294.661 5.53266 310.644 0.177194 328.096 4.49514C358.059 11.9087 396.756 28.7292 432.804 66.0708C468.327 102.868 483.823 140.048 490.288 168.667C494.155 185.785 488.724 201.238 478.056 212.103Z"
           fill="#F8F8F8"
         />
       </g>
       <g filter="url(#filter3_d_19_91)">
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M297.913 462.516C303.789 468.286 311.681 471.077 320.982 468.976C346.064 463.31 379.502 449.583 413.114 417.134C447.017 384.405 462.055 349.567 468.658 322.882C471.069 313.138 468.22 304.703 462.178 298.418C455.953 291.943 446.078 287.582 434.732 287.582L331.774 287.582C307.456 287.582 287.742 307.154 287.742 331.296L287.742 436.291C287.742 447.109 291.831 456.544 297.913 462.516ZM282.431 478.056C271.772 467.59 265.726 452.371 265.726 436.291L265.726 331.296C265.726 295.083 295.297 265.726 331.774 265.726L434.732 265.726C451.594 265.726 467.392 272.184 478.105 283.328C489.001 294.661 494.356 310.644 490.038 328.096C482.625 358.058 465.804 396.755 428.463 432.804C391.665 468.327 354.485 483.823 325.867 490.288C308.749 494.155 293.296 488.724 282.431 478.056Z"
           fill="#F8F8F8"
         />
@@ -59,9 +59,9 @@ export default function Logo() {
           width="235.808"
           height="235.808"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
@@ -93,9 +93,9 @@ export default function Logo() {
           width="235.808"
           height="235.808"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
@@ -127,9 +127,9 @@ export default function Logo() {
           width="235.808"
           height="235.808"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
@@ -161,9 +161,9 @@ export default function Logo() {
           width="235.808"
           height="235.808"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
@@ -196,8 +196,8 @@ export default function Logo() {
           y2="436.019"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#E200B1" />
-          <stop offset="1" stop-color="#6400E2" />
+          <stop stopColor="#E200B1" />
+          <stop offset="1" stopColor="#6400E2" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_19_91"
@@ -207,8 +207,8 @@ export default function Logo() {
           y2="459.117"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#E200B1" />
-          <stop offset="1" stop-color="#6400E2" />
+          <stop stopColor="#E200B1" />
+          <stop offset="1" stopColor="#6400E2" />
         </linearGradient>
         <linearGradient
           id="paint2_linear_19_91"
@@ -218,8 +218,8 @@ export default function Logo() {
           y2="33.9653"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#6400E2" />
-          <stop offset="1" stop-color="#E200B1" />
+          <stop stopColor="#6400E2" />
+          <stop offset="1" stopColor="#E200B1" />
         </linearGradient>
       </defs>
     </svg>
